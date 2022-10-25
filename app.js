bc273a3e9ec1554dd27375e4a7909a1146c47f09
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2022-10-25 22:17:18 +0200

    feat: create playerChoice and computerChoice function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const infoChoice = document.querySelector('.choice-info');
 const userScore = document.querySelector('.user-points');
 const compScore = document.querySelector('.comp-points');
+const gameImages = document.querySelectorAll('.game-field div');
 
 
 let userChoice = "";
@@ -8,7 +9,23 @@ let compChoice = "";
 let userPoints = 0;
 let compPoints = 0;
 
-infoChoice.textContent = "Choose one !"
+infoChoice.textContent = "Choose one !";
 userScore.textContent = userPoints;
 compScore.textContent = compPoints;
+
+
+const playerChoice = (e) => {
+    userChoice = e.target.dataset.option;
+    infoChoice.textContent = `You chose a ${userChoice}!`;
+    
+    computerChoice();
+}
+
+const computerChoice = ()  => {
+    const choiceArray = ['ROCK', 'PAPER', 'SCISSORS'];
+    compChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+}
+
+
+gameImages.forEach((image) => image.addEventListener('click', playerChoice));
 
