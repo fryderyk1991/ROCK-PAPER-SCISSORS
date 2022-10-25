c76cc1d9858c3c2f2aead0ec6323c2196407d99b
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2022-10-25 21:35:12 +0200

    create a variables in js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const infoChoice = document.querySelector('.choice-info');
+const userScore = document.querySelector('.user-points');
+const compScore = document.querySelector('.comp-points');
+
+
+let userChoice = "";
+let compChoice = ""; 
+let userPoints = 0;
+let compPoints = 0;
+
+infoChoice.textContent = "Choose one !"
+userScore.textContent = userPoints;
+compScore.textContent = compPoints;
+
