dcbac5e82747bb293909302488ce865f31e1a6ea
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2022-10-26 12:38:53 +0200

    score of players

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const infoChoice = document.querySelector('.choice-info');
+const infoChoicePlayer = document.querySelector('.choice-info-player');
+const infoChoiceComp = document.querySelector('.choice-info-comp')
 const userScore = document.querySelector('.user-points');
 const compScore = document.querySelector('.comp-points');
 const gameImages = document.querySelectorAll('.game-field div');
@@ -9,36 +10,43 @@ let compChoice = "";
 let userPoints = 0;
 let compPoints = 0;
 
-infoChoice.textContent = "Choose one !";
+infoChoicePlayer.textContent = "Choose one !";
+infoChoiceComp.textContent =  "Comp choice ?";
 userScore.textContent = userPoints;
 compScore.textContent = compPoints;
 
 
 const playerChoice = (e) => {
     userChoice = e.target.dataset.option;
-    infoChoice.textContent = `You chose a ${userChoice}!`;
-    
-    console.log('PLAYER', userChoice)
+    infoChoicePlayer.textContent = `You chose a ${userChoice}!`;
+    gameImages.forEach((image) => image.removeEventListener('click', playerChoice));
     computerChoice();
 }
 
 const gameResult = () => {
     if (userChoice === 'ROCK' && compChoice === 'SCISSORS' || userChoice === 'PAPER' && compChoice === 'ROCK' || userChoice === 'SCISSORS' && compChoice === 'PAPER') {
         console.log('YOU WON')
+        userPoints++;
+        userScore.textContent = userPoints;
     }
     else if (userChoice === compChoice) {
         console.log('DRAW!')
     }
     else {
-        console.log('COMP WON')
+        console.log("COMP WON");
+        compPoints++;
+        compScore.textContent = compPoints;
     }
 }
 
 const computerChoice = ()  => {
-    const choiceArray = ['ROCK', 'PAPER', 'SCISSORS'];
-    compChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
-    console.log('COMP', compChoice)
-    gameResult()
+    setTimeout(() => {
+        const choiceArray = ['ROCK', 'PAPER', 'SCISSORS'];
+        compChoice = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+        infoChoiceComp.textContent = `Comp chose a ${compChoice}`;
+        gameResult()
+        gameImages.forEach((image) => image.addEventListener('click', playerChoice));
+    }, 1000)
 }
 
 
